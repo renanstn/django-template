0b8e41ce4dbdc1c6d269f62519907db5aeb3c9b9
Renan Santana <renan_ota@hotmail.com>
2022-02-22 00:52:50 -0300

    add a simple test

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from myapp import models
 
-# Create your tests here.
+
+class TestModels(TestCase):
+    def test_populate_test_model(self):
+        models.Test.objects.create(name="test")
+        assert models.Test.objects.filter(name="test").exists()
