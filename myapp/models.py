94888466f8086db7e8d17fbe24163808bb167d25
Renan Santana <renan_ota@hotmail.com>
2022-02-22 00:39:45 -0300

    create test model

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Test(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
